d4bede3899d5caf4e568ff0d8826fbf4a7344688
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-04-01 18:00:01 +0500

    bolierplate MUI

---
+++ src/screens/global/Topbar.jsx
@@ -0,0 +1,36 @@
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import { DarkModeOutlined, LightModeOutlined, NotificationsOutlined, PersonOutline, Search, SettingsOutlined } from "@mui/icons-material";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px" >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <Search />
+        </IconButton>
+      </Box>
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ? <DarkModeOutlined /> : <LightModeOutlined />}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutline />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
